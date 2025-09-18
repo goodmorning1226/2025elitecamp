55fb55429b81f0ddbe39044ab570f7e9fe188c2d
goodmorning1226 <allensung.taiwan@gmail.com>
2025-09-18 17:38:43 +0800

    首頁設計修改

--- js/main.js
+++ js/main.js
@@ -1,33 +1,51 @@
 const card_colors = ["#FFF4E2", "#FED7CC", "#FAC2BF", "#B8A9DC", "#58A1CA"];
+const card_titles = ["關於", "參與成員", "行萬里路", "感謝信", "狀元出品"];
+const card_icons = [
+  "../image/icon/about.svg",
+  "../image/icon/members.svg",
+  "../image/icon/trip.svg",
+  "../image/icon/mails.svg",
+  "../image/icon/publications.svg",
+];
 
 const card_container = document.getElementById("card-container");
 
 if (card_container) {
-  card_colors.forEach((color) => {
+  card_titles.forEach((ctitle, idx) => {
     let card = document.createElement("div");
     card.className = "card";
-    card.style.backgroundColor = color;
+    card.style.backgroundColor = card_colors[idx];
     let card_compon1 = document.createElement("div");
     card_compon1.className = "card-compon1";
     card.appendChild(card_compon1);
+
     let card_compon2 = document.createElement("div");
     card_compon2.className = "card-compon2";
+
+    let card_compon2_titlebox = document.createElement("div");
+    card_compon2_titlebox.className = "card-compon2-titlebox";
+    let card_compon2_icon = document.createElement("img");
+    card_compon2_icon.src = card_icons[idx];
+    card_compon2_icon.alt = "icon";
+    card_compon2_icon.className = "card-compon2-img";
+    card_compon2_titlebox.appendChild(card_compon2_icon);
+    let title = document.createElement("h3");
+    title.innerText = ctitle;
+    card_compon2_titlebox.appendChild(title);
+    card_compon2.appendChild(card_compon2_titlebox);
+
+    let card_arrow = document.createElement("img");
+    card_arrow.src = "./image/card_arrow.svg";
+    card_arrow.alt = "card arrow";
+    card_arrow.className = "card-compon2-img";
+    card_compon2.appendChild(card_arrow);
+
     card.appendChild(card_compon2);
     card_container.appendChild(card);
   });
-  card_container.innerHTML += card_container.innerHTML;
 
   const scrollWidth = card_container.scrollWidth / 2;
-  card_container.scrollLeft = scrollWidth / 7; //need to adjust
-
-  card_container.addEventListener("scroll", () => {
-    if (card_container.scrollLeft >= scrollWidth - 0) {
-      card_container.scrollLeft -= scrollWidth;
-    }
-    if (card_container.scrollLeft <= 0) {
-      card_container.scrollLeft += scrollWidth;
-    }
-  });
+  card_container.scrollLeft = scrollWidth / 4; //need to adjust
 }
 
 const reveal = Array.from(
